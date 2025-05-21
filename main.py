d6fb3081bc1bf2bff6a6d643f11d44fc9f9096c9
dan naron <dan@example.com>
2025-05-21 08:35:38 -0500

    add middleware and multiple videos

--- main.py
+++ main.py
@@ -2,14 +2,22 @@ import uvicorn
 from fastapi import FastAPI
 
 from application import router
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
 app.include_router(router)
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 def main():
-    uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
 
 
 if __name__ == "__main__":
